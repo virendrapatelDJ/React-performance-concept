4c225773da68b2c0d2327cb6b5d831c09400763e
Virendra Kumar Patel <virendra.patel@dowjones.com>
2021-12-17 14:33:16 +0530

    use query

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
-import Debounce from './Debounce'
-import Throttle from './Throttle'
+import React, { useState } from 'react';
+import Debounce from './Debounce';
+import Throttle from './Throttle';
+import UseQueryDemo, { WithoutUseQueryDemo } from './UseQuery';
+import { QueryClient, QueryClientProvider } from 'react-query';
+const queryClient = new QueryClient();
 
 export default function Home() {
-    return (
-        <div className='container mt-4 border rounded p-4'>
-            <Debounce/>
-            <hr />
-            <Throttle/>
+  const [show, setShow] = useState(true);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="container mt-4 border rounded p-4">
+        <Debounce />
+        <hr />
+        <Throttle />
+        <hr />
+        <button className="btn btn-success" onClick={() => setShow(!show)}>
+          Toggle Block
+        </button>
+        <hr />
+        <div id="queryExample">
+          {show && (
+            <div className="row">
+              <div className="col border m-2 p-3">
+                <UseQueryDemo />
+              </div>
+              <div className="col border m-2 p-3">
+                <WithoutUseQueryDemo />
+              </div>
+            </div>
+          )}
         </div>
-    )
+      </div>
+    </QueryClientProvider>
+  );
 }
---
+++ app/src/components/UseQuery.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+
+export default function UseQueryDemo() {
+  const query = useQuery('photos', () => {
+    return fetch('https://jsonplaceholder.typicode.com/photos').then(
+      (response) => response.json()
+    );
+  });
+
+  const { data } = query || {};
+  console.log(data);
+  return (
+    <div>
+    {!data?.length && <h1>No Data</h1>}
+      <ul>
+        {data?.map((photo, id) => (
+          <li key={id}>{photo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function WithoutUseQueryDemo() {
+    const [data , setData ] = useState([])
+  useEffect(()=>{
+     fetch('https://jsonplaceholder.typicode.com/photos').then(
+        (response) => response.json()
+      ).then(data=>setData(data))
+  }, [])
+  return (
+    <div>
+        {!data.length && <h1>No Data</h1>}
+      <ul>
+        {data?.map((photo, id) => (
+          <li key={id}>{photo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export { WithoutUseQueryDemo };
